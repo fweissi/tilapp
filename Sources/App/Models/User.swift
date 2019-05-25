e56e0dbd58e2eb582ea4b84a919c370a1184f29e
Keith Weiss <fweissi@me.com>
2019-05-25 13:41:50 -0600

    Added authorization and passwords to project. Now User has a Public facing counterpart for REST calls to hide the password hash.

--- Sources/App/Models/User.swift
+++ Sources/App/Models/User.swift
@@ -31,23 +31,62 @@ import Vapor
 import FluentPostgreSQL
 
 final class User: Codable {
-  var id: UUID?
-  var name: String
-  var username: String
-
-  init(name: String, username: String) {
-    self.name = name
-    self.username = username
-  }
+    var id: UUID?
+    var name: String
+    var username: String
+    var password: String
+    
+    init(name: String, username: String, password: String) {
+        self.name = name
+        self.username = username
+        self.password = password
+    }
+    
+    
+    final class Public: Codable {
+        var id: UUID?
+        var name: String
+        var username: String
+        
+        init(id: UUID?, name: String, username: String) {
+            self.id = id
+            self.name = name
+            self.username = username
+        }
+    }
 }
+
 
 extension User: PostgreSQLUUIDModel {}
 extension User: Content {}
-extension User: Migration {}
+extension User: Migration {
+    static func prepare(on connection: PostgreSQLConnection) -> Future<Void> {
+        return Database.create(self, on: connection, closure: { (builder) in
+            try addProperties(to: builder)
+            builder.unique(on: \.username)
+        })
+        
+    }
+}
 extension User: Parameter {}
 
 extension User {
-  var acronyms: Children<User, Acronym> {
-    return children(\.userID)
-  }
+    var acronyms: Children<User, Acronym> {
+        return children(\.userID)
+    }
+}
+
+extension User.Public: Content {}
+extension User {
+    func convertToPublic() -> User.Public {
+        return User.Public(id: id, name: name, username: username)
+    }
+}
+extension Future where T: User {
+    func convertToPublic() -> Future<User.Public> {
+        return self.map(to: User.Public.self) {
+            user in
+            return user.convertToPublic()
+        }
+    }
 }
